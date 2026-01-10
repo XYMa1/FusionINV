b850e4de7bb7cf0c14b8b6db1d867b5dc7045bb5
Ma Xiangyang <107552404888@stu.xju.edu.cn>
2026-01-10 16:22:23 +0800

    Modify get_stable_diffusion_model function
    Updated the return type of the get_stable_diffusion_model function to FusionINVAttentionStableDiffusionPipeline and added requires_safety_checker parameter.

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -3,13 +3,17 @@ from diffusers import DDIMScheduler
 
 from models.stable_diffusion_baseline import FusionINVAttentionStableDiffusionPipeline
 from models.unet_2d_condition import FreeUUNet2DConditionModel
+#from models.stable_diffusion import CrossImageAttentionStableDiffusionPipeline
 
 
-def get_stable_diffusion_model() -> CrossImageAttentionStableDiffusionPipeline:
+def get_stable_diffusion_model() -> FusionINVAttentionStableDiffusionPipeline:
     print("Loading Stable Diffusion model...")
     device = torch.device(f'cuda') if torch.cuda.is_available() else torch.device('cpu')
-    pipe = FusionINVAttentionStableDiffusionPipeline.from_pretrained("pretrained/stable-diffusion-v1-5",
-                                                                      safety_checker=None).to(device)
+    pipe = FusionINVAttentionStableDiffusionPipeline.from_pretrained(
+        "pretrained/stable-diffusion-v1-5",
+        safety_checker=None,
+        requires_safety_checker=False  # 添加这个参数
+    ).to(device)
     pipe.unet = FreeUUNet2DConditionModel.from_pretrained("pretrained/stable-diffusion-v1-5", subfolder="unet").to(device)
     pipe.scheduler = DDIMScheduler.from_config("pretrained/stable-diffusion-v1-5", subfolder="scheduler")
 
